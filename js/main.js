c9fd13351655b27ce3dee097ee090a53a2199155
SergioT388 <sergio.tola.et28@gmail.com>
2024-04-24 22:14:27 -0300

    Modifiqué JS

--- js/main.js
+++ js/main.js
@@ -2,6 +2,5 @@ let nombreProducto = "heladera"
 let precioUnitario = 30000
 let cantidadDeseada = prompt("Ingrese la cantidad de heladeras que quiere")
 let costoFinal = precioUnitario * cantidadDeseada
-log(cantidadDeseada)
 
 alert(costoFinal)
