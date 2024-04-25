e17eed4836ba0b1cab2510b814227a83a48e0fdb
SergioT388 <sergio.tola.et28@gmail.com>
2024-04-24 22:08:21 -0300

    Estuve realizando el Desafío 1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nombreProducto = "heladera"
+let precioUnitario = 30000
+let cantidadDeseada = prompt("Ingrese la cantidad de heladeras que quiere")
+let costoFinal = precioUnitario * cantidadDeseada
+log(cantidadDeseada)
+
+console.log(costoFinal)
+
+alert(costoFinal)
