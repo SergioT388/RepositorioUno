8f3004ab4a5d1e24a176cb66fd0affee507b83f8
SergioT388 <sergio.tola.et28@gmail.com>
2024-05-16 21:13:25 -0300

    Desafío 3

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ let nombreProducto = "heladera"
 let precioUnitario = 30000
 let cantidadDeseada = prompt("Ingrese la cantidad de heladeras que quiere")
 let costoFinal = precioUnitario * cantidadDeseada
-
-alert(costoFinal)
+let descuento = costoFinal * 0.1
+let costoDescuento = costoFinal - descuento
+if(cantidadDeseada > 5) {
+console.log(costoDescuento);
+} else{
+    console.log(costoFinal);
+}
